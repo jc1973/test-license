e571c133b3ccb5c36c2f353dfd3b91677dd0daab
John Carter <john.carter@odin360.com>
2019-04-15 13:00:33 +0100

    basic project backup

---
+++ vars/testProject.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/env groovy
+
+
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+   
+  pipeline {
+    agent any
+    options {
+      timeout(time: config.timeout )
+       
+    }
+     
+    stages {
+       
+      stage("Clean workspace") {
+        steps {
+          cleanWs() 
+        }
+      }
+       
+      stage('Build App') {
+        steps {
+          dir(config.directory) {
+            // testing
+            sh "ls"
+            sh "pwd"
+            // nodejs(nodeJSInstallationName: body.nodeJSVersion) {
+              // sh "npm install --registry ${params.nexusHost}"
+              // sh "npm install --registry ${params.nexusHost}"
+              // sh "npm install --registry ${params.nexusHost}"
+              // sh "npm install --registry ${params.nexusHost}"
+            // }  
+          }
+        }
+      }
+       
+      stage("Unit Tests") {
+        steps {
+          dir('client') {
+            // testing
+            sh "ls"
+            sh "pwd"
+            // nodejs(nodeJSInstallationName: body.nodeJSVersion) {
+              //   sh "npm test"
+            // }
+          }
+        }
+      }
+       
+       
+       
+    }
+  }
+}
