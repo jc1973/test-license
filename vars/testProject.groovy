3d75dc4a5e8db2c6c98d7fa3b142e716dcdbb585
John Carter <john.carter@odin360.com>
2019-04-16 10:54:26 +0100

    Changing stage names and environment

--- vars/testProject.groovy
+++ vars/testProject.groovy
@@ -11,17 +11,35 @@ def call(body) {
     agent any
     options {
       timeout(time: config.timeout )
-       
     }
+
+  environment {
+    ARTIFACT="${JOB_BASE_NAME}-${BUILD_NUMBER}.zip"
+    NPM_CONFIG_USERCONFIG='/var/lib/jenkins/.npmrc.nexus'
+    NEXUS_URL="${config.nexus_url}"
+    NEXUS_CREDS="${config.nexus_creds}"
+    NODE_VERSION="NodeJS ${config.node_version}"
+  }
      
     stages {
        
-      stage("Clean workspace") {
+      stage('Tag commit') {
         steps {
-          cleanWs() 
+          withCredentials([usernamePassword(credentialsId: config.nexus_creds, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+            sh '''
+            #!/bin/bash
+            # REPO=$(echo ${GIT_URL} | sed 's/https:..//g')
+            # REPO=$(echo ${GIT_URL} | sed 's/https:..//g')
+            # echo git tag ${BRANCH_NAME}-${BUILD_NUMBER} ${GIT_COMMIT}
+            # echo git tag ${BRANCH_NAME}-${BUILD_NUMBER} ${GIT_COMMIT}
+            # echo git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO} --tags
+            # echo git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO} --tags
+            '''
+          }
         }
       }
-       
+
+
       stage('Build App') {
         steps {
           dir(config.directory) {
@@ -44,6 +62,10 @@ def call(body) {
             // testing
             sh "ls"
             sh "pwd"
+            sh 'echo #### jenkins variables ###'
+            sh 'printenv'
+            sh 'echo ### Environment Variables:'
+            sh 'env'
             // nodejs(nodeJSInstallationName: body.nodeJSVersion) {
               //   sh "npm test"
             // }
@@ -56,3 +78,6 @@ def call(body) {
     }
   }
 }
+
+
+
